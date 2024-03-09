c4994239f7e259b13a834df68f211c997aea0a25
Aaron Kieran Southcombe <aaronkieransouthcombe@gmail.com>
2024-03-09 19:34:42 +0100

    Moved broker to mod and finished registration logic.

---
+++ src/broker.rs
@@ -0,0 +1,26 @@
+use tokio::sync::mpsc;
+use crate::{BrokerMessage, ClientMessage};
+use std::collections::HashMap;
+use std::sync::atomic::{AtomicU32, Ordering};
+
+static CLIENT_ID_COUNTER: AtomicU32 = AtomicU32::new(0);
+
+pub async fn message_broker(mut globalbroker_reciever: mpsc::UnboundedReceiver<BrokerMessage>) {
+    let mut clients:  HashMap<u32, mpsc::UnboundedSender<ClientMessage>> = HashMap::new();
+    let mut client_names: HashMap<u32, String> = HashMap::new();
+
+    while let Some(message) = globalbroker_reciever.recv().await {
+        match message {
+            BrokerMessage::Register{ client_name, client_chan }  => {
+                let new_id = CLIENT_ID_COUNTER.fetch_add(1, Ordering::SeqCst);
+                client_chan.send(ClientMessage::ClientId(new_id));
+                clients.insert(new_id, client_chan);
+                client_names.insert(new_id, client_name);
+                    ()
+            }
+            BrokerMessage::Unregister(client) => {}
+            BrokerMessage::Command { command } => {}
+            BrokerMessage::Message { content } => {}
+        };
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,56 @@
 use tokio::net::{TcpListener, TcpStream};
 use std::net::SocketAddr;
+use tokio::sync::mpsc;
 use tokio::io;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
+
+mod broker;
+
+enum BrokerMessage {
+    Register { client_name: String, client_chan: mpsc::UnboundedSender<ClientMessage>},
+    Unregister(Client),
+    Command { command: String },
+    Message { content: String },
+}
+
+#[derive(Debug)]
+enum ClientState {
+    Registered(u32),
+    Unregistered,
+}
+
+enum ClientMessage {
+    ClientId(u32),
+    Message { content: String },
+    CommandResponse { command_response: String },
+}
+
+struct Client {
+    id: u32,
+    sender: mpsc::UnboundedSender<ClientMessage>,
+}
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
 
     // First create a TcpListener that will listen for connections.
     let listener = TcpListener::bind("127.0.0.1:8000").await?;
+    
+    let (globalbroker_sender, globalbroker_reciever) = mpsc::unbounded_channel::<BrokerMessage>();
+
+    tokio::spawn(async move {
+        broker::message_broker(globalbroker_reciever).await;
+    });
 
     loop {
         // Here, the listener matches new connections to their Ok and Err states, and acts
         // accordingly.
+        let client_sender = globalbroker_sender.clone();
         match listener.accept().await {
             // If Ok, MOVE the TcpStream created from the listener.accept into the handle_client,
             // along with the corresponding addr (plus error handling).
             Ok((client_stream, client_addr)) => { tokio::spawn(async move {
-                match handle_client(client_stream, client_addr).await {
+                match handle_client(client_stream, client_addr, client_sender).await {
                     Ok(_) => println!("Handled successfully"),
                     Err(_) => eprintln!("Client handle failed")
                 }
@@ -29,10 +63,12 @@ async fn main() -> io::Result<()> {
 }
 
 
-async fn handle_client(mut client_stream: TcpStream, client_addr: SocketAddr) -> io::Result<()> {
+async fn handle_client(mut client_stream: TcpStream, client_addr: SocketAddr, client_sender: mpsc::UnboundedSender<BrokerMessage>) -> io::Result<()> {
 
+    let mut state = ClientState::Unregistered;
     // First, declare a buffer to negotiate client traffic.
     let mut buffer = vec![0; 1024];
+    let (broker_sender, mut client_receiver) = mpsc::unbounded_channel();
    
     // Handle the text logic, TO-DO broker
     while let Ok(client_bytes) = client_stream.read(&mut buffer).await {
@@ -43,14 +79,27 @@ async fn handle_client(mut client_stream: TcpStream, client_addr: SocketAddr) ->
 
         if let Ok(client_text) = std::str::from_utf8(&buffer[..client_bytes]) {
             let trim_newline = client_text.trim_end_matches('\n');
-            println!("{}: {}", client_addr.to_string(), trim_newline);
+            match state {
+                ClientState::Unregistered => {
+                    let _ = client_sender.send(BrokerMessage::Register{client_name: trim_newline.to_string(), client_chan: broker_sender.clone()});
+                    while let Some(message) = client_receiver.recv().await {
+                        match message {
+                            ClientMessage::ClientId(new_id) => {
+                                state = ClientState::Registered(new_id);
+                                println!("state {:?}", state);
+                                ()
+                            }
+                            _ => {}
+                        }
+                    }
+                                
+                } 
+                ClientState::Registered(new_id) => {}
+            };
+                                       
         } else {
             eprintln!("Text is not valid utf8")
         }
     }
     Ok(())
-}
-
-async fn message_broker(mut client_stream: TcpStream) {
-
 }
